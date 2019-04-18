97a19a092328e0cd8afeea3aa2eb0f17e4a723a6
haowei1 <2278395159@qq.com>
2019-04-18 20:02:58 +0800

    update

---
+++ app/src/main/java/com/android/hao/db/MyDatabaseHelper.java
@@ -0,0 +1,28 @@
+package com.android.hao.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+import android.widget.Toast;
+
+public class MyDatabaseHelper extends SQLiteOpenHelper {
+
+    private static final String TAG = "MyDatabaseHelper";
+
+    public MyDatabaseHelper(Context context) {
+        super(context, "test.db", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table Test(id integer primary key autoincrement, username varchar(20), password varchar(20))");
+        Log.i(TAG, "onCreate: create successful");
+    }
+    
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
